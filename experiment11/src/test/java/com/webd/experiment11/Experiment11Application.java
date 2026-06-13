a644f91ff5a06e53bf919a1b2aa7103ef6481d5b
RashiChomal2007 <rashi.25b01010125@abes.ac.in>
2026-06-13 18:07:00 +0530

    exp11

---
+++ experiment11/src/test/java/com/webd/experiment11/Experiment11Application.java
@@ -0,0 +1,12 @@
+package com.webd.experiment11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Experiment11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Experiment11Application.class, args);
+    }
+}
